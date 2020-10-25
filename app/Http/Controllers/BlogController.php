20f0df8b973915c72e52e76e6713528d2ded66df
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-25 23:39:22 +0900

    to show blog detail

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
 	/**
 	* ブログ一覧を表示
+	* @param int $id
 	* @return view
 	*/
 
     public function showList(){
-    	return view('blog.list');
+    	$blogs = Blog::all();
+    	return view('blog.list', ['blogs' => $blogs]);
+    }
+
+    // ブログ詳細を表示
+    public function showDetail($id){
+    	$blog = Blog::find($id);
+
+    	if(is_null($blog)){
+    		\Session::flash('err_msg', 'データがありません');
+    		return redirect(route('blogs'));
+    	}
+
+    	return view('blog.detail',['blog' => $blog]);
     }
 }
