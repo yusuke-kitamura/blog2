65724aaa16d89a5e52de63b9a2c9400b4bccf0d3
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-25 23:39:56 +0900

    add detail page

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -5,19 +5,26 @@
 <div class="row">
   <div class="col-md-8 col-md-offset-2">
       <h2>ブログ記事一覧</h2>
+      @if(session('err_msg'))
+        <p class="text-danger">
+          {{ session('err_msg') }}
+        </p>
+      @endif
+
       <table class="table table-striped">
           <tr>
               <th>記事番号</th>
               <th>日付</th>
               <th>タイトル</th>
-              <th></th>
           </tr>
+
+          @foreach($blogs as $blog)
           <tr>
-              <td>1</td>
-              <td>2020/06/30</td>
-              <td>テスト</td>
-              <td></td>
+              <td>{{ $blog->id }}</td>
+              <td>{{ $blog->updated_at }}</td>
+              <td><a href="/blog/{{ $blog->id }}">{{ $blog->title }}</a></td>
           </tr>
+          @endforeach
       </table>
   </div>
 </div>
