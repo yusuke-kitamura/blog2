1c69b1dd9c4a4ee9a002da963ab7b5a5fa02aafc
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-25 17:21:14 +0900

    add controller to show blogs

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+	/**
+	* ブログ一覧を表示
+	* @return view
+	*/
+
+    public function showList(){
+    	return view('blog.list');
+    }
+}
