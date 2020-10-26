4b508c0360b792158f9cbcbbd13bd53c306b74d3
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-26 15:33:22 +0900

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ブログ一覧画面を表示
+Route::get('/','BlogController@showList')->name('blogs');
+
+//登録画面を表示
+Route::get('/blog/create','BlogController@showCreate')->name('create');
+
+// ブログ登録
+Route::post('/blog/store','BlogController@exeStore')->name('store');
+
+
+// ブログ詳細を表示
+Route::get('/blog/{id}','BlogController@showDetail')->name('show');
+
+// ブログの編集画面を表示
+Route::get('/blog/edit/{id}','BlogController@showEdit')->name('edit');
+Route::post('/blog/update','BlogController@exeupdate')->name('update');
+
+// ブログ削除機能
+Route::post('/blog/delete/{id}','BlogController@exeDelete')->name('delete');
