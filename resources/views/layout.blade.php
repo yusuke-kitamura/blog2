b67e5d61a9069b323a9310f8c315685da826aaff
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-26 10:08:36 +0900

    add blade to create blogs

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/app.js" defer></script>
