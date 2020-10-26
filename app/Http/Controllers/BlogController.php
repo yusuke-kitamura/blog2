92fe3a2b9050a832844ee163f18567ba45809d7c
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-10-26 15:34:31 +0900

    action to delete blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -102,4 +102,21 @@ class BlogController extends Controller
 		\Session::flash('err_msg', 'ブログを更新しました');
     	return redirect(route('blogs'));
     }
+
+    // ブログ削除
+    public function exeDelete($id){
+    	if(empty($id)){
+    		\Session::flash('err_msg', 'データがありません');
+    		return redirect(route('blogs'));
+    	}
+
+    	try{
+	    	Blog::destroy($id);
+    	}catch(\Throwable $e){
+			abort(500);
+    	}
+
+    	\Session::flash('err_msg', '削除しました');
+    	return redirect(route('blogs'));
+    }
 }
